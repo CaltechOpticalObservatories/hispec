13f5fefeca32672e03c652a781bbc5024bb18c28
Kupaianaha <163094900+Kupaianaha@users.noreply.github.com>
2025-06-09 14:57:54 -0700

    Reviewer Comments and Mods implemented - Elijah.AB (#33)
    * Reviewer Comments and Mods implemented - Elijah.AB
    
    * slight changes to the loop controls; full ability to command and qurey all stage proided states. -Elijah.AB
    
    * slight changes to the loop controls; full ability to command and qurey all stage proided states. -Elijah.AB
    
    * Loop control and Query Changes -Elijah.AB
    
    * Help String changes - Elijah.AB
    
    * Help String changes - Elijah.AB
    
    * Some formatting changes to follow pep8 a better for readability. -Elijah.AB

--- hispec/tests/util/thorlabs/gimbal/test_gimbal_mount.py
+++ hispec/tests/util/thorlabs/gimbal/test_gimbal_mount.py
@@ -15,9 +15,9 @@ class Test_GimbalMount(unittest.TestCase):
                                     -Checks Error Handling
         '''
         # Checks "getter" function: gets state of enable
-        self.dev = PPC102_Coms(inifile, log=False)
+        self.dev = PPC102_Coms(IP='192.168.29.100', port = 10013)
         self.dev.open()
-        ret = self.dev.get_enable()
+        ret = self.dev.get_enable(channel = 1)
         assert ret == DATA_CODES.CHAN_ENABLED or ret == DATA_CODES.CHAN_DISABLED
         # Checks "setter" function; sets state of enable
         for ch in [1,2]:
@@ -31,15 +31,23 @@ class Test_GimbalMount(unittest.TestCase):
         self.assertFalse(self.dev.set_enable(channel=-1))
         self.assertFalse(self.dev.set_enable(enable=-1))
         self.assertFalse(self.dev.set_enable(enable=3))
+        self.assertTrue(self.dev.set_enable(enable = 2))
+        ret = self.dev.get_enable()
+        assert ret[0] == DATA_CODES.CHAN_DISABLED
+        assert ret[1] == DATA_CODES.CHAN_DISABLED
+        self.assertTrue(self.dev.set_enable(enable = 1))
+        ret = self.dev.get_enable()
+        assert ret[0] == DATA_CODES.CHAN_ENABLED
+        assert ret[1] == DATA_CODES.CHAN_ENABLED
         self.dev.close()
-        time.sleep(.1)
+        time.sleep(.25)
         with self.assertRaises(Exception):
             self.dev.get_enable()
             self.dev.set_enable()
-        time.sleep(.1)
+        time.sleep(.25)
     
     def test_loop(self):
-        self.dev = PPC102_Coms(inifile, log=False)
+        self.dev = PPC102_Coms(IP='192.168.29.100', port = 10013)
         time.sleep(.2)
         self.dev.open()
         time.sleep(.25)
@@ -52,57 +60,73 @@ class Test_GimbalMount(unittest.TestCase):
             assert self.dev.set_loop(channel=ch, loop=1)
             ret = self.dev.get_loop(channel=ch)
             assert ret == DATA_CODES.OPEN_LOOP
-        self.assertFalse(self.dev.set_loop(channel=3))
+        self.assertFalse(self.dev.set_loop(channel=5))
         self.assertFalse(self.dev.set_loop(channel=-1))
         self.assertFalse(self.dev.set_loop(loop=-1))
-        self.assertFalse(self.dev.set_loop(loop=3))
+        self.assertFalse(self.dev.set_loop(loop=10))
+        self.assertTrue(self.dev.set_loop(loop = 4))
+        ret = self.dev.get_loop(channel = 0)
+        assert ret[0] == DATA_CODES.CLOSED_LOOP
+        assert ret[1] == DATA_CODES.CLOSED_LOOP
+        self.assertTrue(self.dev.set_loop(loop = 3))
+        ret = self.dev.get_loop(channel = 0)
+        assert ret[0] == DATA_CODES.OPEN_LOOP
+        assert ret[1] == DATA_CODES.OPEN_LOOP
+        self.assertTrue(self.dev.set_loop(loop = 2))
+        ret = self.dev.get_loop(channel = 0)
+        assert ret[0] == DATA_CODES.CLOSED_LOOP
+        assert ret[1] == DATA_CODES.CLOSED_LOOP
+        self.assertTrue(self.dev.set_loop(loop = 1))
+        ret = self.dev.get_loop(channel = 0)
+        assert ret[0] == DATA_CODES.OPEN_LOOP
+        assert ret[1] == DATA_CODES.OPEN_LOOP
         self.dev.close()
-        time.sleep(.1)
+        time.sleep(.25)
         with self.assertRaises(Exception):
             self.dev.get_loop()
             self.dev.set_loop()
-        time.sleep(.1)
+        time.sleep(.25)
 
     def test_max_voltages(self):
         #Max Voltage
-        self.dev = PPC102_Coms(inifile, log=False)
+        self.dev = PPC102_Coms(IP='192.168.29.100', port = 10013)
         self.dev.open()
         time.sleep(.25)
         for ch in [1,2]:
             ret = self.dev.get_max_output_voltage(channel=ch)
-            assert self.dev.set_max_output_voltage(channel=ch, limit=1000)
+            assert self.dev.set_max_output_voltage(channel=ch, limit=100)
             ret = self.dev.get_max_output_voltage(channel=ch)
-            assert ret == 1000
-            assert self.dev.set_max_output_voltage(channel=ch, limit=1500)
+            assert ret == 100
+            assert self.dev.set_max_output_voltage(channel=ch, limit=150)
             ret = self.dev.get_max_output_voltage(channel=ch)
-            assert ret == 1500
-        self.assertFalse(self.dev.set_max_output_voltage(channel=3, limit=1000))
-        self.assertFalse(self.dev.set_max_output_voltage(channel=2, limit=2500))
-        self.assertFalse(self.dev.set_max_output_voltage(channel=0, limit=1000))
-        self.assertFalse(self.dev.set_max_output_voltage(channel=-1, limit=2000))
-        self.assertFalse(self.dev.set_max_output_voltage(channel=1, limit=-1000))
+            assert ret == 150
+        self.assertFalse(self.dev.set_max_output_voltage(channel=3, limit=100))
+        self.assertFalse(self.dev.set_max_output_voltage(channel=2, limit=250))
+        self.assertFalse(self.dev.set_max_output_voltage(channel=0, limit=100))
+        self.assertFalse(self.dev.set_max_output_voltage(channel=-1, limit=200))
+        self.assertFalse(self.dev.set_max_output_voltage(channel=1, limit=-100))
         self.dev.close()
         time.sleep(0.1)
         with self.assertRaises(Exception):
             self.dev.get_max_output_voltage(channel=1)
-            self.dev.set_max_output_voltage(channel=1,limit=500)
-        time.sleep(.1)
+            self.dev.set_max_output_voltage(channel=1,limit=50)
+        time.sleep(.25)
         return
     
     def test_positions(self):
         #CLOSED LOOP CONTROL
-        self.dev = PPC102_Coms(inifile, log=False)
-        time.sleep(.1)
+        self.dev = PPC102_Coms(IP='192.168.29.100', port = 10013)
+        time.sleep(.25)
         self.dev.open()
-        time.sleep(.1)
+        time.sleep(.25)
         for ch in [1,2]:
             self.dev.set_loop(channel=ch,loop=2)
             self.dev.set_position(channel=ch,pos=100)
             res = self.dev.get_position(channel=ch)
-            assert 95 < res < 105
+            assert 90 < res < 110
             self.dev.set_position(channel=ch,pos=1000)
             res = self.dev.get_position(channel=ch)
-            assert 995 < res < 1005
+            assert 990 < res < 1010
             self.dev.set_position(channel=ch,pos=31000)
             res = self.dev.get_position(channel=ch)
             assert 30995 < res < 31005
@@ -115,12 +139,12 @@ class Test_GimbalMount(unittest.TestCase):
         self.assertFalse(self.dev.set_position(channel=3,pos=100))
         self.assertFalse(self.dev.get_position(channel=0))
         self.dev.close()
-        time.sleep(.1)
+        time.sleep(.25)
         return
 
     def test_output_voltage(self):
         #OPEN LOOP CONTROL
-        self.dev = PPC102_Coms(inifile, log=False)
+        self.dev = PPC102_Coms(IP='192.168.29.100', port = 10013)
         self.dev.open()
         for ch in [1,2]:
             self.dev.set_loop(channel=ch,loop=2)
@@ -140,7 +164,7 @@ class Test_GimbalMount(unittest.TestCase):
         self.assertFalse(self.dev.set_output_volts(channel=3,volts=100))
         self.assertFalse(self.dev.get_output_volts(channel=0))
         self.dev.close()
-        time.sleep(.1)
+        time.sleep(.25)
         return
 
     def test_isupper(self):
