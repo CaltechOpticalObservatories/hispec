4273f6ee95cf25372cc262f8fdac732abe1d332f
ElijahAB <elijahab@caltech.edu>
2025-06-30 12:52:59 -0700

    Removed the error codes class, unused because it is only usable in the api that linux has no access to. -Elijah.AB

---
+++ hispec/util/standa/8SMC.py
@@ -0,0 +1,6 @@
+#NOTE:: Pip install of libximc is needed to use the library imported
+#       These are not standard python librarys but are on PyPI
+#                       -Elijah Anakalea-Buckley
+
+import libximc
+
