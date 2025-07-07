c430f8bbb4334c60dbaea0ae5d9c32ab2dc35e89
ElijahAB <elijahab@caltech.edu>
2025-07-07 11:25:48 -0700

    Impleneted Library based on documentation. Mock, Dev and Error checking still needed. -Elijah.AB

--- hispec/util/standa/8SMC.py
+++ hispec/util/standa/8SMC.py
@@ -35,8 +35,9 @@ class SMC(object):
         # get coms
         self.IP = IP
         self.port = port
+        self.step_size_coeff = 0.0025  # Example conversion coefficient, adjust as needed(mm)
         self.dev_open = False
-
+        self.axis = ximc.Axis(self.IP, self.port)
 
     def open(self):
         '''
@@ -66,6 +67,14 @@ class SMC(object):
             self.command_read_setting = self.axis.command_read_settings()
             #get device information
             self.device_information = self.axis.get_device_information()
+            #get and save engine settings
+            self.engine_settings = self.axis.get_engine_settings()
+            #Set calb for user units
+            self.axis.set_calb(self.step_size_coeff, self.axis.engine_settings.MicrosetpMode)
+            #Set limits TODO: Check if this is correct when having the device
+            self.limits = self.engine_settings.Limits
+            self.min_limit = self.limits.MinLimit
+            self.max_limit = self.limits.MaxLimit
 
             if self.logger:#Log that device is open
                 self.logger.info("Device opened successfully.")
@@ -120,6 +129,8 @@ class SMC(object):
             -Will Reference and move to the last valid position.
             return: bool on successful ref
             libximc::
+
+            TODO:: Determine if this is needed, as it is not in the libximc docs
         '''
         #Check if connection not open
         if not self.dev_open:
@@ -155,9 +166,22 @@ class SMC(object):
             return False
 
         #Try to home to zero or parked position
+        try:
+            self.axis.command_homezero()
+            self.axis.command_wait_for_stop(100)
+            #Check position after homing
+            position = self.axis.get_position()
+            if self.logger:
+                self.logger.info("Stage homed to position: %s", position.Position)   
             #return true if succesful
-        #catch error
-            #log error and return false
+            return True
+        #catch error  
+        except Exception as e:
+            #log error
+            if self.logger:
+                self.logger.error("Error homing stage: %s", str(e))
+            #return false if unsuccessful
+            return False
 
     def move_abs(self, position):
         '''
@@ -166,7 +190,7 @@ class SMC(object):
             - Check min_limit and max_limit for valid inputs
             parameters: min_limit < int:"position" < max_limit
             return: bool on successful or unsuccessful absolute move
-            libximc:: command_move()
+            libximc:: command_move_calb()
         '''
         #Check if connection not open
         if not self.dev_open:
@@ -176,21 +200,42 @@ class SMC(object):
             return False
 
         #Try move absolute
+        try:
             #check limits/valid inputs
+            if position < self.min_limit or position > self.max_limit:
+                if self.logger:
+                    self.logger.error("Position out of limits: %s", position)
+                return False
+            #move absolute
+            self.axis.command_move_calb(position)
+            self.axis.command_wait_for_stop(100)
 
-            #after move is done, check position with acceptable error
+            #after move is done, check position
+            position = self.axis.get_position_calb()
+            if position.Error != 0:
+                if self.logger:
+                    self.logger.error("Error moving stage: %s", position.Error)
+                return False
+            else: 
+                if self.logger:
+                    self.logger.info("Stage moved to position: %s", position.Position)
                 #return true if succesful
+                return True
         #catch error
+        except Exception as e:
             #log error and return false
+            if self.logger:
+                self.logger.error("Error moving stage: %s", str(e))
+            return False
 
-    def move_rel(self, position):
+    def move_rel(self, position:float):
         '''
             Move the stage to a RELATIVE position. Send stage to a position
                 relative to its current position.
             - Check min_limit and max_limit for range of device
             parameters: min_limit < (current_position+int:"position") < max_limit
             return: bool on successful or unsuccessful relative move
-            libximc:: command_movr()
+            libximc:: command_movr_calb()
         '''
         #Check if connection not open
         if not self.dev_open:
@@ -200,12 +245,38 @@ class SMC(object):
             return False
 
         #Try move relative
+        try:
             #check limits/valid inputs
+            #get current position
+            current_position = self.axis.get_position_calb().Position
+            #calculate new position
+            position = current_position + position
+            #check if new position is within limits
+            if position < self.min_limit or position > self.max_limit:
+                if self.logger:
+                    self.logger.error("Position out of limits: %s", position)
+                return False
+            #move relative
+            self.axis.command_movr_calb(position)
+            self.axis.command_wait_for_stop(100)
 
-            #after move is done, check position with acceptable error
+            #after move is done, check position
+            position = self.axis.get_position_calb()
+            if position.Error != 0:
+                if self.logger:
+                    self.logger.error("Error moving stage: %s", position.Error)
+                return False
+            else: 
+                if self.logger:
+                    self.logger.info("Stage moved to position: %s", position.Position)
                 #return true if succesful
+                return True
         #catch error
+        except Exception as e:
             #log error and return false
+            if self.logger:
+                self.logger.error("Error moving stage: %s", str(e))
+            return False
 
     def get_position(self):
         '''
@@ -221,9 +292,22 @@ class SMC(object):
             return False
 
         #Try get_position
+        try:
+            #get position
+            position = self.axis.get_position_calb()
             #return aspects of the position object
+            position_string = f"Position: {position.Position}, " \
+                              f"Error: {position.Error}, " \
+                              f"Moving: {position.Moving}"
+            if self.logger:
+                self.logger.info(position_string)
+            return position.Position, position_string
         #catch error
+        except Exception as e:
             #log error and return None
+            if self.logger:
+                self.logger.error("Error getting position: %s", str(e))
+            return None
 
     def status(self):
         '''
@@ -240,17 +324,29 @@ class SMC(object):
             return False
 
         #Try status function    
+        try:
+            #get status
+            status = self.axis.get_status_calb()
             #parse results
+            status_string = f"Status: {status.Status}, Position: {status.Position}, " \
+                            f"Error: {status.Error}, Moving: {status.Moving}"
             #return status in user friendly way
+            if self.logger:
+                self.logger.info(status_string)
+            return status_string
         #catch error
+        except Exception as e:
             #log error and return false
+            if self.logger:
+                self.logger.error("Error getting status: %s", str(e))
+            return None
 
     def halt(self):
         '''
             IMMITATELY halts the stage, no matter the status or if moving, stage
                 stops(for safety purposes)
             return: status of the stage(log and/or print hald command called)
-            libximc::
+            libximc:: command_stop()
         '''
         #Check if connection not open
         if not self.dev_open:
@@ -260,6 +356,19 @@ class SMC(object):
             return False
 
         #Try imidiate stop of stage
+        try:
+            self.axis.command_stop()
+            #Check status after halting
+            status = self.axis.get_status_calb()
+            #TODO:: Finish the halt check
+            #status.Moving
+            if self.logger:
+                self.logger.info("Stage halted successfully.")
             #return true if succesful
+            return True
         #catch error
+        except Exception as e:
             #log error and return false
+            if self.logger:
+                self.logger.error("Error halting stage: %s", str(e))
+            return False
